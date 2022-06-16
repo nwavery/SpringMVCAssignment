9caeeafd4332ac0a7ca9b1288d35459c349c41f7
Nathan Avery <nwavette@gmail.com>
2022-06-16 17:56:13 -0500

    First commit

---
+++ src/test/java/com/assignments/assignment/SpringMvcAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignments.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
